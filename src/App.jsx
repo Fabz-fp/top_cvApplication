75c91ffec7f8b8b341eb07d4f36d8f0639580995
Fabian Ferreira <fab14an.fp@gmail.com>
2026-05-27 20:59:59 -0400

    feat: add educational experience section style: update styles for education section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './styles/App.css';
 import GeneralInfo from './components/GeneralInfo';
+import Education from './components/Education';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>CV Application</h1>
 
       <GeneralInfo />
+      <Education />
     </main>
   );
 }
---
+++ src/components/Education.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+
+function Education() {
+    const [isEditing, setIsEditing] = useState(true);
+
+    const [educationInfo, setEducationInfo] = useState({
+        school: '',
+        studyTitle: '',
+        studyDate: ''
+    });
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+
+        setEducationInfo({
+            ...educationInfo,
+            [name]: value,
+        });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsEditing(false);
+    }
+
+    function handleEdit() {
+        setIsEditing(true);
+    }
+
+    if (isEditing) {
+        return (
+            <section>
+                <h2>Educational Experience</h2>
+
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="school">School Name</label>
+                        <input 
+                            type="text"
+                            id='school'
+                            name='school'
+                            value={educationInfo.school}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="studyTitle">Title of Study</label>
+                        <input
+                            type="text"
+                            id="studyTitle"
+                            name="studyTitle"
+                            value={educationInfo.studyTitle}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="studyDate">Date of Study</label>
+                        <input
+                            type="date"
+                            id="studyDate"
+                            name="studyDate"
+                            value={educationInfo.studyDate}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <button type='submit'>Submit</button>
+                </form>
+            </section>
+        );
+    }
+
+    return (
+        <section>
+            <h2>Educational Experience</h2>
+
+            <p>
+                <strong>School:</strong> {educationInfo.school}
+            </p>
+
+            <p>
+                <strong>Title of Study:</strong> {educationInfo.studyTitle}
+            </p>
+
+            <p>
+                <strong>Date:</strong> {educationInfo.studyDate}
+            </p>
+
+            <button onClick={handleEdit}>Edit</button>
+        </section>
+    );
+
+}
+
+export default Education;
